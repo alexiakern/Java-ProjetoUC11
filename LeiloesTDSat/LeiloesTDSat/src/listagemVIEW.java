3cee449f2a8d61e72e770893a9629fb25f7b2c6f
Aléxia Kern <145412393+alexiakern@users.noreply.github.com>
2024-01-21 01:14:36 -0300

    Implementar funcionalidade do botão vender

--- LeiloesTDSat/LeiloesTDSat/src/listagemVIEW.java
+++ LeiloesTDSat/LeiloesTDSat/src/listagemVIEW.java
@@ -138,9 +138,7 @@ public class ListagemVIEW extends javax.swing.JFrame {
     private void btnVenderActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVenderActionPerformed
         String id = id_produto_venda.getText();
         
-        ProdutosDAO produtosdao = new ProdutosDAO();
-        
-        //produtosdao.venderProduto(Integer.parseInt(id));
+        ProdutosDAO.venderProduto(Integer.parseInt(id));
         listarProdutos();
     }//GEN-LAST:event_btnVenderActionPerformed
 
